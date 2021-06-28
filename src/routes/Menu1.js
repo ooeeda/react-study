f0d6e8a5c8d9a67d3f136e5851434d50a6b36910
박수환 <ooeeda@gmail.com>
2021-06-28 23:34:50 +0900

    데이터 정렬 페이징처리로 갈아업기전 백업

--- src/routes/Menu1.js
+++ src/routes/Menu1.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import "../routes/menu.css";
 
 function Menu1(){
+    const [number, setNumber] = useState(0);
     return(
         <section className="container">
             <div className="menu">
+                <h1>{number}</h1>
                 <h1>Menu1</h1>
             </div>
         </section>
--- src/routes/Menu2.js
+++ src/routes/Menu2.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
+import {Button} from "@material-ui/core";
 //6a26a9cb6a2ad32917b41bfa057bf667
 
 function Menu2(){
+     const [number, setNumber] = useState(0);
+
+
+    const onIncrease = () => {
+        setNumber(number => number + 1);
+        //console.log(number);
+        //setNumber(prevNumber => prevNumber + 1);
+    }
+    const onDecrease = () => {
+        setNumber(number => number - 1);
+        // console.log(number);
+        //setNumber(prevNumber => prevNumber -1 );
+    }
+
+
 
     return(
 
         <section className="container">
             <div className="menu">
                 <h1>menu2</h1>
+                <h1>{number}</h1>
+                <Button onClick={onIncrease}>+</Button>
+                <Button onClick={onDecrease}>-</Button>
             </div>
         </section>
 
